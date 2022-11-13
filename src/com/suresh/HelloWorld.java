597406f065171819800af5c6eb1c212fbac65709
SureshKumarReddi <sureshkumarreddyraina@gmail.com>
2022-11-13 13:01:08 +0530

    Revert "todays' commit"
    This reverts commit cc8c6ae0eafed842116b005748fa376713267cd9.
    Now we are reverting to the last commit just for checking the revert command working or not`

--- src/com/suresh/HelloWorld.java
+++ src/com/suresh/HelloWorld.java
@@ -8,7 +8,6 @@ System.out.println("helloworld");
 System.out.println("new line");
 System.out.println("new line added through elipse to demo the merge conflicts");
 		System.out.println("again added new line");
-		System.out.println("we have to be strong enough");
 	}
 
 }
